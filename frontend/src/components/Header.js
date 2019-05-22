e22bf5a4ca5853436adc96a8359a30069de8af7c
UsuallyChris <schoonc@gmail.com>
2019-05-22 15:44:43 -0700

    Layed out component structure and did some basic styling

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div>
+          <h2>Lead Manager</h2>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header
