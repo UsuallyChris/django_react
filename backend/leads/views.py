0b317ec326f7a5a305d99fdc7c6c4938b813b0d9
UsuallyChris <schoonc@gmail.com>
2019-05-21 22:33:18 -0700

    Updated view definitions for leads app

--- backend/leads/views.py
+++ backend/leads/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+""" view definitions for leads app """
+from rest_framework import viewsets, permissions
+from .models import Lead
+from .serializers import LeadSerializer
 
-# Create your views here.
+
+class LeadViewSet(viewsets.ModelViewSet):
+    """ ViewSet for the Lead model """
+    queryset = Lead.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = LeadSerializer
