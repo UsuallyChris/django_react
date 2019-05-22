24797a2e55059fa604d98fbdc690e976722eebf9
UsuallyChris <schoonc@gmail.com>
2019-05-21 22:00:12 -0700

    Created Lead model and ran migrations

--- backend/leads/models.py
+++ backend/leads/models.py
@@ -1,3 +1,10 @@
+""" model definitions for leads app"""
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    """ Lead model definition """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
