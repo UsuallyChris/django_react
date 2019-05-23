ce80164dc94e806287c369b81f4304bc3c4f2b1f
UsuallyChris <schoonc@gmail.com>
2019-05-22 21:50:07 -0700

    Created table view for all leads

--- frontend/src/components/Leads.js
+++ frontend/src/components/Leads.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types'
 import { getLeads } from '../actions/leads';
@@ -14,9 +14,31 @@ class Leads extends Component{
 
   render() {
     return (
-      <div>
-        <h1>Leads List</h1>
-      </div>
+      <Fragment>
+        <h2>Leads</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map(lead => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td><button>Delete</button></td>
+              </tr>  
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     )
   }
 }
@@ -26,4 +48,5 @@ const mapStateToProps = state => ({
   leads: state.leads.leads,
 })
 
+//  connect([if it needs to pull down state, make a mapStateToProps function. if not then make this null], { [any actions needed. import at top of file] })(Component)
 export default connect(mapStateToProps, { getLeads })(Leads);
