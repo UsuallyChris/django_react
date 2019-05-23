0aaaf7f8e90ba51139f0f711813c9d6cb8a40f8e
UsuallyChris <schoonc@gmail.com>
2019-05-22 22:10:12 -0700

    Added base form that logs to console

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class Form extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState( {
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log('Submit');
+  }
+
   render() {
     return (
       <div>
-        <h1>Add Lead Form</h1>
+        <h2>Add Lead</h2>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="">Name</label>
+          <input type="text" name="name" onChange={this.onChange} value={this.state.name}/>
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" onChange={this.onChange} value={this.state.email}/>
+          <label htmlFor="">Message</label>
+          <input type="text" name="message" onChange={this.onChange} value={this.state.message}/>
+          <button type="submit">Add Lead</button>
+        </form>
       </div>
     )
   }
